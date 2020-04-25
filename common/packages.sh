a5483e00ad6101ded7c72fa41078a01227c5e1fa
marek <m.necesany@choosit.com>
2020-04-25 05:52:48 +0200

    keeping 7.3-fpm-alpine only

--- common/packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://get.symfony.com/cli/installer -o - | bash
-mv /home/$IMAGE_USER/.symfony/bin/symfony /usr/local/bin/symfony
-
-apt-get update && apt-get upgrade
-
-pecl install grpc && docker-php-ext-enable grpc
