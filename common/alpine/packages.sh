95df5d8dff97aa4eb28d3196996e3b7a6f82c818
marek <m.necesany@choosit.com>
2020-04-25 05:35:53 +0200

    Dockerfiles cleanup - keep 7.3-fpm only

--- common/alpine/packages.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://get.symfony.com/cli/installer -o - | bash
-mv /home/$IMAGE_USER/.symfony/bin/symfony /usr/local/bin/symfony
-
-apk --update --no-cache add linux-headers libstdc++ musl php7-common
-
-pecl install grpc && docker-php-ext-enable grpc
