b671533c0c7fd3978f3213e3a59b3422e9b95911
marek <m.necesany@choosit.com>
2020-06-29 16:19:22 +0200

    fix - new java install attempt

--- common/packages.sh
+++ common/packages.sh
@@ -6,7 +6,12 @@ mv /home/$IMAGE_USER/.symfony/bin/symfony /usr/local/bin/symfony
 apt-get update && apt-get upgrade
 
 curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java
+source "$HOME/.sdkman/bin/sdkman-init.sh" && \
+    yes | sdk install java && \
+    rm -rf $HOME/.sdkman/archives/* && \
+    rm -rf $HOME/.sdkman/tmp/*
+
+JAVA_HOME="/home/$IMAGE_USER/.sdkman/candidates/java/current"
+PATH="$JAVA_HOME/bin:$PATH"
 
 pecl install grpc && docker-php-ext-enable grpc
