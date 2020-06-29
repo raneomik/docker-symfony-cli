6be3af8317113ba15e95298e943f25a4a8bddbce
marek <m.necesany@choosit.com>
2020-06-29 12:01:07 +0200

    fix - 7.* - deps updates - jre installs addition

--- common/packages.sh
+++ common/packages.sh
@@ -4,6 +4,6 @@ curl https://get.symfony.com/cli/installer -o - | bash
 mv /home/$IMAGE_USER/.symfony/bin/symfony /usr/local/bin/symfony
 
 apt-get update && apt-get upgrade
-apt-get install java
+apt-get install -y default-jre
 
 pecl install grpc && docker-php-ext-enable grpc
