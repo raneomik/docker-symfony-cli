73b178d46ca465fb64a3b557d001690eb524267f
marek <m.necesany@choosit.com>
2020-07-08 14:49:24 +0200

    fix - 7.* java deps - symbolic link for java bin

--- common/packages.sh
+++ common/packages.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
+apt-get update && apt-get upgrade
+
 curl https://get.symfony.com/cli/installer -o - | bash
 mv $HOME/.symfony/bin/symfony /usr/local/bin/symfony
-
-apt-get update && apt-get upgrade
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh" && \
     yes | sdk install java && \
     rm -rf $HOME/.sdkman/archives/* && \
     rm -rf $HOME/.sdkman/tmp/*
-cp $HOME/.sdkman/candidates/java/current/bin/java /usr/local/bin/
+ln -sf $HOME/.sdkman/candidates/java/current/bin/java /usr/local/bin/java
 
 export JAVA_HOME="$HOME/.sdkman/candidates/java/current"
 export PATH="$JAVA_HOME/bin:$PATH"
+
+chown -R $IMAGE_USER:$IMAGE_USER $HOME
 
 pecl install grpc && docker-php-ext-enable grpc
